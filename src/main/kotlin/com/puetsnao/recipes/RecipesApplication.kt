dd8f8dfb6f204bf7d7d10f200751c75ca9154c1f
Santiago Kiryluk Maclean <santiago.kiryluk@gmail.com>
2025-08-06 20:33:44 +0200

    [Ticket-x] Initialize Spring Boot project with Kotlin, Gradle setup, and basic configuration files.

---
+++ src/main/kotlin/com/puetsnao/recipes/RecipesApplication.kt
@@ -0,0 +1,11 @@
+package com.puetsnao.recipes
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RecipesApplication
+
+fun main(args: Array<String>) {
+	runApplication<RecipesApplication>(*args)
+}
