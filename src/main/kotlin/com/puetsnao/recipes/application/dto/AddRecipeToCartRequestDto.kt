523936dac4edef5cd84d526a38bd921ec7989ca9
Santiago Kiryluk Maclean <santiago.kiryluk@gmail.com>
2025-08-07 15:52:02 +0200

    [Ticket-x] Add validation for positive Recipe ID in `AddRecipeToCartRequestDto`.

--- src/main/kotlin/com/puetsnao/recipes/application/dto/AddRecipeToCartRequestDto.kt
+++ src/main/kotlin/com/puetsnao/recipes/application/dto/AddRecipeToCartRequestDto.kt
@@ -1,8 +1,10 @@
 package com.puetsnao.recipes.application.dto
 
 import jakarta.validation.constraints.NotNull
+import jakarta.validation.constraints.Positive
 
 data class AddRecipeToCartRequestDto(
     @field:NotNull(message = "Recipe ID is required")
+    @field:Positive(message = "Recipe ID must be a positive number")
     val recipeId: Long?
 )
